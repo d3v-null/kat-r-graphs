7fe4ab190597d5958a0bcd137548410f5f3b63fa
derwentx <derwentx@gmail.com>
2016-10-11 22:49:28 +1100

    fixed the stupid categorical data bug with a hack
    https://github.com/yhat/ggplot/issues/555

--- interaction_plot.py
+++ interaction_plot.py
@@ -1,10 +1,29 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
-import ggplot as gp
+from ggplot import *
 
-data_fname = 'locof.csv'
+import string
 
-locof_df = pd.read_csv(data_fname)
+# from copy import copy
 
 g = gp.ggplot(gp.aes(x='sex', y='dist', color='line'), data=locof_df)
+data_path = 'locof.csv'
+
+locof_df = pd.read_csv(data_path)
+
+def stupid_sex_plotting_hack(df, _aes=None):
+    if not _aes: _aes = aes(x='sex', y='dist')
+    x_axis = _aes['x']
+    df[x_axis] = df[x_axis].str.translate(string.maketrans("MF", "01")).astype('int64')
+    g = ggplot(df, _aes)
+    g+= scale_x_discrete(name='Gender',breaks=[0, 1],labels=['Male', 'Female'])
+    # g+= scale_x_discrete(name='Gender',breaks=[0, 1],labels=['♂ Male', '♀ Female'])
+    return g
+
+locof_lines_df = locof_df.groupby(['sex', 'line'],as_index=False).mean()
+
+g = stupid_sex_plotting_hack(locof_lines_df, aes(x='sex', y='dist', group='line'))
+g+= geom_point()
+g+= geom_line()
 g.show()
